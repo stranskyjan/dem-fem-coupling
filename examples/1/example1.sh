e511d915d468b55c1956379b9fe246cb3f5b1a15
Jan Stránský <jan.stransky@fsv.cvut.cz>
2017-06-22 09:50:13 +0200

    code cleanup in examples/1

--- examples/1/example1.sh
+++ examples/1/example1.sh
@@ -1,10 +1,11 @@
 NAME=example1
-OOFEMBASE=/tmp/$NAME-oofem
-RUN=true
-PV=true
-PROFILE=true
-MESH=false
+
+MESH=false   # create a mesh by salome (optional)
+PROFILE=true # profile times spend on individual simulation parts
+PV=false     # run python paraview postprocessing
+
 if $MESH; then
+	OOFEMBASE=/tmp/$NAME-oofem
 	salome -t $NAME-unv.py
 	python $NAME-unv2oofem.py $OOFEMBASE
 	cp $OOFEMBASE.in $NAME-oofem.in
